d93591275a94105a9c73159019a7f141ebb4a5ff
Zander <clucasa@onid.orst.edu>
2013-04-03 10:13:18 -0700

    Working on Iofx rendering

--- ApexTest/ApexParticles.cpp
+++ ApexTest/ApexParticles.cpp
@@ -8,12 +8,12 @@ ApexParticles::ApexParticles() :
     mEmitterModule(0),
     mIofxModule(0)
 {
-	return;
+    return;
 }
 
 ApexParticles::~ApexParticles()
 {
-	return;
+    return;
 }
 
 void ApexParticles::Init(NxApexSDK* gApexSDK)
@@ -23,26 +23,26 @@ void ApexParticles::Init(NxApexSDK* gApexSDK)
 
 
     mParticleIosModule = static_cast<NxModuleParticleIos*>(gApexSDK->createModule("ParticleIOS", &errorCode));
-	checkErrorCode(&errorCode);
+    checkErrorCode(&errorCode);
     PX_ASSERT(mParticleIosModule);
     if(mParticleIosModule)
     {
         NxParameterized::Interface* params = mParticleIosModule->getDefaultModuleDesc();
-	    mParticleIosModule->init(*params);
+        mParticleIosModule->init(*params);
     }
 
     
     mIofxModule = static_cast<NxModuleIofx*>(gApexSDK->createModule("IOFX", &errorCode));
     checkErrorCode(&errorCode);
-	PX_ASSERT(mIofxModule);
-	if (mIofxModule)
-	{
-		NxParameterized::Interface* params = mIofxModule->getDefaultModuleDesc();
-		mIofxModule->init(*params);
+    PX_ASSERT(mIofxModule);
+    if (mIofxModule)
+    {
+        NxParameterized::Interface* params = mIofxModule->getDefaultModuleDesc();
+        mIofxModule->init(*params);
 
-		//m_apexIofxModule->disableCudaInterop();
-		//m_apexIofxModule->disableCudaModifiers();
-	}
+        //m_apexIofxModule->disableCudaInterop();
+        //m_apexIofxModule->disableCudaModifiers();
+    }
 
     mEmitterModule = static_cast<NxModuleEmitter*> ( gApexSDK->createModule("Emitter", &errorCode));
     checkErrorCode(&errorCode);
@@ -50,21 +50,21 @@ void ApexParticles::Init(NxApexSDK* gApexSDK)
     if(mEmitterModule)
     {
         NxParameterized::Interface* params = mEmitterModule->getDefaultModuleDesc();
-	    mEmitterModule->init(*params);
+        mEmitterModule->init(*params);
         PxU32 numScalables = mEmitterModule->getNbParameters();
-	    NxApexParameter** m_emitterModuleScalables = mEmitterModule->getParameters();
-	    for (physx::PxU32 i = 0; i < numScalables; i++)
-	    {
-		    NxApexParameter& p = *m_emitterModuleScalables[i];
-		    mEmitterModule->setIntValue(i, p.range.maximum);
-	    }
+        NxApexParameter** m_emitterModuleScalables = mEmitterModule->getParameters();
+        for (physx::PxU32 i = 0; i < numScalables; i++)
+        {
+            NxApexParameter& p = *m_emitterModuleScalables[i];
+            mEmitterModule->setIntValue(i, p.range.maximum);
+        }
     }
 
 }
 
 void ApexParticles::CreateEmitter(NxApexSDK* gApexSDK, NxApexScene* gApexScene)
 {
-    NxParticleIosAssetAuthoring* particleAuthoring = static_cast<NxParticleIosAssetAuthoring*> (gApexSDK->createAssetAuthoring(NX_PARTICLE_IOS_AUTHORING_TYPE_NAME));
+    /*NxParticleIosAssetAuthoring* particleAuthoring = static_cast<NxParticleIosAssetAuthoring*> (gApexSDK->createAssetAuthoring(NX_PARTICLE_IOS_AUTHORING_TYPE_NAME));
     NxIofxAssetAuthoring* iofxAuthoring = static_cast<NxIofxAssetAuthoring*> (gApexSDK->createAssetAuthoring(NX_IOFX_AUTHORING_TYPE_NAME));
     
     NxParameterized::Interface* iosParams = particleAuthoring->releaseAndReturnNxParameterizedInterface();
@@ -79,36 +79,61 @@ void ApexParticles::CreateEmitter(NxApexSDK* gApexSDK, NxApexScene* gApexScene)
     NxParameterized::Interface* iofxAssetNamedRef = 0;
     iofxAssetNamedRef->initDefaults();
     iofxAssetNamedRef->setName(iofxAsset->getAssetNxParameterized()->name());
+*/
 
-
-    NxApexAssetAuthoring* assetauthoring = gApexSDK->createAssetAuthoring(NX_APEX_EMITTER_AUTHORING_TYPE_NAME);
-    NxParameterized::Interface* asParams = assetauthoring->releaseAndReturnNxParameterizedInterface();
+    //NxApexAssetAuthoring* assetauthoring = gApexSDK->createAssetAuthoring(NX_APEX_EMITTER_AUTHORING_TYPE_NAME);
+    //NxParameterized::Interface* asParams = assetauthoring->releaseAndReturnNxParameterizedInterface();
   
-	// Set the asset's type
-    NxParameterized::setParamRef(*asParams, "iofxAssetName", iofxAssetNamedRef);
-    NxParameterized::setParamRef(*asParams, "iosAssetName", iosAssetNamedRef);
-	
-	NxApexEmitterAsset* emitterAsset = static_cast<NxApexEmitterAsset*> (gApexSDK->createAsset(asParams, "sphere_emitter"));
+    // Set the asset's type
+    //NxParameterized::setParamRef(*asParams, "iofxAssetName", iofxAssetNamedRef);
+    //NxParameterized::setParamRef(*asParams, "iosAssetName", iosAssetNamedRef);
     
-	NxParameterized::Interface* descParams = emitterAsset->getDefaultActorDesc();
-	
-	PX_ASSERT(descParams);
-	if (!descParams)
-	{
-		return;
-	}
-	// Set Actor pose
-	//NxParameterized::setParamMat44( *descParams, "initialPose", pose );
-	NxApexEmitterActor* emitterActor;
-	if(descParams->areParamsOK())
-	{
-		emitterActor = static_cast<NxApexEmitterActor*>(emitterAsset->createApexActor(*descParams,*gApexScene));
-		if(emitterActor)
-		{
-			emitterActor->setCurrentPosition(PxVec3(0.0f, 5.0f, 0.0f));
-			emitterActor->startEmit( true );
-		}
-	}
+    NxApexEmitterAsset* emitterAsset;
+    physx::apex::NxApexAsset* asset = reinterpret_cast<physx::apex::NxApexAsset*>(gApexSDK->getNamedResourceProvider()->getResource(NX_APEX_EMITTER_AUTHORING_TYPE_NAME, "testSpriteEmitter4ParticleFluidIos"));
+    if (asset)
+    {
+        emitterAsset = static_cast<NxApexEmitterAsset*> (asset);
+    }
+    //NxApexEmitterAsset* emitterAsset = static_cast<NxApexEmitterAsset*> (gApexSDK->createAsset(asParams, "testMeshEmitter4ParticleIos.apb"));
+    gApexSDK->forceLoadAssets();
+
+    NxParameterized::Interface* descParams = emitterAsset->getDefaultActorDesc();
+    PX_ASSERT(descParams);
+    if (!descParams)
+    {
+        return;
+    }
+
+    // Set Actor pose
+    //NxParameterized::setParamMat44( *descParams, "initialPose", pose );
+    NxApexEmitterActor* emitterActor;
+    if(descParams->areParamsOK())
+    {
+        emitterActor = static_cast<NxApexEmitterActor*>(emitterAsset->createApexActor(*descParams,*gApexScene));
+        if(emitterActor)
+        {
+            emitterActor->setCurrentPosition(PxVec3(0.0f, 1.0f, 0.0f));
+            emitterActor->startEmit( true );
+            //emitterActor->setLifetimeRange(physx::apex::NxRange<PxF32>(1,5));
+            //emitterActor->setRateRange(physx::apex::NxRange<PxF32>(10, 10));
+        }
+    }
+
+    PxBounds3 b;
+    b.setInfinite();
+
+    mRenderVolume = mIofxModule->createRenderVolume(*gApexScene, b, 0, true );
+    emitterActor->setPreferredRenderVolume( mRenderVolume );
+}
+
+bool ApexParticles::RenderVolume(physx::apex::NxUserRenderer & renderer)
+{
+    mRenderVolume->lockRenderResources();
+  
+    mRenderVolume->updateRenderResources(false);
+    mRenderVolume->dispatchRenderResources(renderer);
+
+    mRenderVolume->unlockRenderResources();
 }
 
 bool ApexParticles::checkErrorCode(NxApexCreateError* err)
